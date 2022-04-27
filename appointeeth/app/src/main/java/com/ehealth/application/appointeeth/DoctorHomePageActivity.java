427b946450b82718b5d20de65091c9736d0abb26
Razvan-Alexandru Nicu <razvnicu@amazon.com>
2022-04-27 22:28:46 +0300

    Add Pacient/Doctor homepage layouts and logout button

--- appointeeth/app/src/main/java/com/ehealth/application/appointeeth/DoctorHomePageActivity.java
+++ appointeeth/app/src/main/java/com/ehealth/application/appointeeth/DoctorHomePageActivity.java
@@ -2,13 +2,30 @@ package com.ehealth.application.appointeeth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.ehealth.application.appointeeth.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DoctorHomePageActivity extends AppCompatActivity {
+
+    private Button logoutButton;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_home_page);
+
+        // Initializari
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        logoutButton = findViewById(R.id.LogoutButton);
+        logoutButton.setOnClickListener(v -> {
+            mFirebaseAuth.signOut();
+            startActivity(new Intent(DoctorHomePageActivity.this, LoginActivity.class));
+        });
     }
 }
--- appointeeth/app/src/main/java/com/ehealth/application/appointeeth/PacientHomePageActivity.java
+++ appointeeth/app/src/main/java/com/ehealth/application/appointeeth/PacientHomePageActivity.java
@@ -2,13 +2,30 @@ package com.ehealth.application.appointeeth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.ehealth.application.appointeeth.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PacientHomePageActivity extends AppCompatActivity {
+
+    private Button logoutButton;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pacient_home_page);
+
+        // Initializari
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        logoutButton = findViewById(R.id.LogoutButton);
+        logoutButton.setOnClickListener(v -> {
+            mFirebaseAuth.signOut();
+            startActivity(new Intent(PacientHomePageActivity.this, LoginActivity.class));
+        });
     }
 }
